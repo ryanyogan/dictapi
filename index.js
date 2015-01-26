4892cfbf92fc862cc2eb4762955912019aae5ca1
Ryan Yogan <ryanyogan@gmail.com>
2015-01-26 00:28:49 -0600

    Added the api into the server file, added deps

--- index.js
+++ index.js
@@ -2,15 +2,12 @@ var koa    = require('koa');
 var app    = koa();
 var router = require('koa-router');
 var mount  = require('koa-mount');
+var api    = require('./api/api');
 
-var handler = function *(next) {
-  this.type = 'json';
-  this.status = 200;
-  this.body = {'Welcome': 'This is a not express :)'};
-};
 
 var APIv1 = new router();
-APIv1.get('/all', handler);
+APIv1.get('/all', api.all)
+APIv1.get('/single', api.single);
 
 app.use(mount('/v1', APIv1.middleware()));
 
