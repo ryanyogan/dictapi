3c6812626e458a306c6a70fe51c3dc2b3020d6dd
Ryan Yogan <ryanyogan@gmail.com>
2015-01-26 00:18:32 -0600

    Updated the package manifest to use routing

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-var koa = require('koa');
-var app = koa();
+var koa    = require('koa');
+var app    = koa();
+var router = require('koa-router');
+var mount  = require('koa-mount');
 
-app.use(function *(next) {
+var handler = function *(next) {
   this.type = 'json';
   this.status = 200;
   this.body = {'Welcome': 'This is a not express :)'};
-});
+};
+
+var APIv1 = new router();
+APIv1.get('/all', handler);
+
+app.use(mount('/v1', APIv1.middleware()));
 
 if (!module.parent) app.listen(3000);
 console.log('Application is running on port: 3000');
